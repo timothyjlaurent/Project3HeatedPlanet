8cba4dae39fc1a615bc88779ded1104a5eb811aa
software-engineering <ubuntu@engineering.(none)>
2014-11-26 19:03:17 -0500

    some simulation bug fixes

--- PlanetSim/src/models/EarthCell.java
+++ PlanetSim/src/models/EarthCell.java
@@ -2,113 +2,123 @@ package models;
 
 /**
  * Contains information about a cell on the Earth
+ * 
  * @author johnh_000
- *
+ * 
  */
-public class EarthCell implements ICell {
+public class EarthCell {
 
 	private double northSideLength;
-	
+
 	private double southSideLength;
-	
+
 	private double verticalSideLength;
-	
+
 	private double surfaceArea;
-	
+
 	private double temperature;
-	
+
 	private double percentOfRadiation;
-	
+
 	/**
 	 * Instantiates a new instance of the EarthCell class
-	 * @param northSideLength the length of the north side of the cell
-	 * @param southSideLength the length of the south side of the cell
-	 * @param verticalSideLength the length of the vertical side of the cell
-	 * @param temperature the temperature of the cell
-	 * @param percentOfRadiation percent of total radiation the cell receives
-	 * @param surfaceArea the surface area of the cell
+	 * 
+	 * @param northSideLength
+	 *            the length of the north side of the cell
+	 * @param southSideLength
+	 *            the length of the south side of the cell
+	 * @param verticalSideLength
+	 *            the length of the vertical side of the cell
+	 * @param temperature
+	 *            the temperature of the cell
+	 * @param percentOfRadiation
+	 *            percent of total radiation the cell receives
+	 * @param surfaceArea
+	 *            the surface area of the cell
 	 */
-	public EarthCell(double northSideLength, double southSideLength, double verticalSideLength, double temperature, double percentOfRadiation, double surfaceArea)
-	{
+	public EarthCell(final double northSideLength, final double southSideLength, final double verticalSideLength, final double temperature, final double percentOfRadiation, final double surfaceArea) {
 		this.northSideLength = northSideLength;
 		this.southSideLength = southSideLength;
 		this.verticalSideLength = verticalSideLength;
 		this.temperature = temperature;
 		this.percentOfRadiation = percentOfRadiation;
 		this.surfaceArea = surfaceArea;
-		
+
 		return;
 	}
 
 	/**
 	 * Gets the length of the North Side of the cell in meters
+	 * 
 	 * @return length of the north side of the cell
 	 */
-	public double NorthSideLength()
-	{
-		return this.northSideLength;
+	public double NorthSideLength() {
+		return northSideLength;
 	}
 
 	/**
 	 * Gets the length of the South Side of the cell in meters
+	 * 
 	 * @return length of the south side of the cell
 	 */
-	public double SouthSideLength()
-	{
-		return this.southSideLength;
+	public double SouthSideLength() {
+		return southSideLength;
 	}
 
 	/**
 	 * Gets the length of the Vertical Sides of the cell in meters
+	 * 
 	 * @return length of the vertical sides of the cell
 	 */
-	public double VerticalSideLength()
-	{
-		return this.verticalSideLength;
+	public double VerticalSideLength() {
+		return verticalSideLength;
 	}
-	
+
 	/**
 	 * Gets the surface area of the cell in meters squared
+	 * 
 	 * @return the surface area of the cell
 	 */
-	public double SurfaceArea()
-	{
-		return this.surfaceArea;
+	public double SurfaceArea() {
+		return surfaceArea;
 	}
 
 	/**
 	 * Gets the current temperature of the cell in kelvin
+	 * 
 	 * @return the current temperature of the cell
 	 */
-	public double Temperature()
-	{
-		return this.temperature;
+	public double Temperature() {
+		return temperature;
 	}
 
 	/**
-	 * Gets the angle used to compute the amount of radiation on the cell in degrees
+	 * Gets the angle used to compute the amount of radiation on the cell in
+	 * degrees
+	 * 
 	 * @return angle of the cell's radiation
 	 */
-	public double PercentOfRadiation()
-	{
-		return this.percentOfRadiation;
+	public double PercentOfRadiation() {
+		return percentOfRadiation;
 	}
 
 	/**
 	 * Sets the Temperature of the cell in Kelvin
-	 * @param the new current temperature of the cell
+	 * 
+	 * @param the
+	 *            new current temperature of the cell
 	 */
-	public void Temperature(double temperature)
-	{
+	public void Temperature(final double temperature) {
 		this.temperature = temperature;
 	}
 
 	/**
 	 * Sets the angle of radiation for the cell in degrees
-	 * @param angle of radiation
+	 * 
+	 * @param angle
+	 *            of radiation
 	 */
-	public void PercentOfRadiation(double percentOfRadiation)
-	{
+	public void PercentOfRadiation(final double percentOfRadiation) {
 		this.percentOfRadiation = percentOfRadiation;
 	}
 }
--- PlanetSim/src/util/SimulationUtil.java
+++ PlanetSim/src/util/SimulationUtil.java
@@ -1,11 +1,16 @@
 package util;
 
+import static constants.SimulationConstants.CIRCUMFERENCE_OF_EARTH;
 import static constants.SimulationConstants.DEGREES_ROTATED_PER_MINUTE;
 import static constants.SimulationConstants.MINUTES_IN_YEAR;
+import static constants.SimulationConstants.SURFACEAREA_OF_EARTH;
 import static constants.SimulationConstants.WIDTH_IN_DEGREES;
+import static java.lang.Math.abs;
+import static java.lang.Math.cos;
 import static java.lang.Math.min;
 import static java.lang.Math.pow;
 import static java.lang.Math.sqrt;
+import static java.lang.Math.toRadians;
 
 import java.util.Date;
 import java.util.HashMap;
@@ -14,6 +19,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
+import models.Experiment;
 import models.GridPoint;
 import models.SimulationStats;
 
@@ -42,16 +48,16 @@ public class SimulationUtil {
 		return sqrt(temp / total);
 	}
 
-	public static SimulationStats calculateSimulationStats(final Set<GridPoint> gridPoints) {
+	public static SimulationStats calculateSimulationStats(final Set<GridPoint> gridPoints, final Experiment experiment) {
 		final SimulationStats stats = new SimulationStats();
 
 		double sum = 0;
 		for (final GridPoint gridPoint : gridPoints) {
 			stats.setMin(min(stats.getMin(), gridPoint.getTemperature()));
 			stats.setMax(Math.max(stats.getMax(), gridPoint.getTemperature()));
-			sum += gridPoint.getTemperature();
+			sum += gridPoint.getTemperature() * calculateSurfaceArea(experiment, gridPoint);
 		}
-		stats.setMean(sum / gridPoints.size());
+		stats.setMean(sum / SURFACEAREA_OF_EARTH);
 		stats.setStandardDeviation(calculateStandardDeviation(gridPoints, stats.getMean()));
 		return stats;
 	}
@@ -89,4 +95,30 @@ public class SimulationUtil {
 		}
 		return set;
 	}
+
+	public static double calculateVerticalSideLength(final Experiment experiment) {
+		final double circ = CIRCUMFERENCE_OF_EARTH;
+		final double spacing = experiment.getSimulationSettings().getGridSpacing();
+		final double width = WIDTH_IN_DEGREES;
+		final double vlength = circ * spacing / width;
+		return vlength;
+	}
+
+	public static double calculateSurfaceArea(final Experiment experiment, final GridPoint gridPoint) {
+		final double verticalSideLength = calculateVerticalSideLength(experiment);
+		final double northSideLength = calculateNorthSideLength(experiment, gridPoint, verticalSideLength);
+		final double southSideLength = calculateSouthSideLength(gridPoint, verticalSideLength);
+		final double height = sqrt(pow(verticalSideLength, 2) - .25 * pow(northSideLength - southSideLength, 2));
+		final double surfaceArea = .5 * (northSideLength + southSideLength) * height;
+		return surfaceArea;
+	}
+
+	public static double calculateNorthSideLength(final Experiment experiment, final GridPoint gridPoint, final double verticalSideLength) {
+		return abs(cos(toRadians(gridPoint.getTopLatitude() + experiment.getSimulationSettings().getGridSpacing())) * verticalSideLength);
+	}
+
+	public static double calculateSouthSideLength(final GridPoint gridPoint, final double verticalSideLength) {
+		return abs(cos(toRadians(gridPoint.getTopLatitude())) * verticalSideLength);
+	}
+
 }
